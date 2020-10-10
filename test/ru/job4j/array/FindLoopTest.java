c2fc05ef585bd6f92185efb1ffa41aa8adc6dcfb
dim0nster <32572277+dim0nster@users.noreply.github.com>
2020-10-11 02:31:24 +0300

    6.5. Поиск индекса в диапазоне. [#298081]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -4,23 +4,32 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class FindLoopTest {
 
     @Test
-    public void whenArrayHas4Then1() {
-        int[] input = new int[] {5, 4, 3, 2};
-        int value = 4;
-        int result = FindLoop.indexOf(input, value);
-        int expect = 1;
+    public void whenFindMinus1() {
+        int[] input = new int[] {1, 2, 30, 6, 5, 10};
+        int value = 30;
+        int start = 3;
+        int finish = 5;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 
     @Test
-    public void whenArrayHas1ThenM1() {
-        int[] input = new int[] {5, 4, 3, 2};
-        int value = 1;
-        int result = FindLoop.indexOf(input, value);
-        int expect = -1;
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
