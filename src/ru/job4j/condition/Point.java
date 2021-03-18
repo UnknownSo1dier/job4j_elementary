eaaa459eabb526555a37f1439615cca1522b0fb3
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-03-18 22:47:18 +0300

    4. Расстояние между точками в трехмерном пространстве. [#298243]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -8,19 +8,33 @@ public class Point {
 
     private int y;
 
+    private int z;
+
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this(x, y);
+        this.z = z;
     }
 
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
     public static void main(String[] args) {
-        Point a = new Point(0, 1);
-        Point b = new Point(1, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+        Point a = new Point(1, 2, 4);
+        Point b = new Point(0, 3,10);
+        Point c = new Point(6, 6, 6);
+        double rslAB = a.distance3d(b);
+        double rslAC = a.distance3d(c);
+        double rslBC = b.distance3d(c);
+        System.out.println(rslAB + " " + rslAC + " "+ rslBC);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -19,4 +19,17 @@ public class PointTest {
         assertThat(rslAC, closeTo(3, 0.001));
         assertThat(rslBC, closeTo(5, 0.001));
     }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(1, 2, 4);
+        Point b = new Point(0, 3,10);
+        Point c = new Point(6, 6, 6);
+        double rslAB = a.distance3d(b);
+        double rslAC = a.distance3d(c);
+        double rslBC = b.distance3d(c);
+        assertThat(rslAB, closeTo(6.164, 0.001));
+        assertThat(rslAC, closeTo(6.708, 0.001));
+        assertThat(rslBC, closeTo(7.810, 0.001));
+    }
 }
