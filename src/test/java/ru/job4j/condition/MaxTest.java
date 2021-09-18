a3325a5f4f08116f88e2612fcc8d1882660d6691
dim0nster <32572277+dim0nster@users.noreply.github.com>
2021-09-18 12:48:37 +0300

    Add Maven Elementary debug

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -7,19 +7,19 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 
     @Test
-    public void when_1_2_then_2() {
+    public void when12then2() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
 
     @Test
-    public void when_5_10_2_then_10() {
+    public void when5102then10() {
         int result = Max.max(5, 10, 2);
         assertThat(result, is(10));
     }
 
     @Test
-    public void when_10_10_60_100_then_100() {
+    public void when101060100then100() {
         int result = Max.max(10, 10, 60, 100);
         assertThat(result, is(100));
     }
