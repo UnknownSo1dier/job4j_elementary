87075c83965416682f0b331bfa9d61ccb4d27b59
Dmitry <dfirst282@gmail.com>
2024-08-18 17:46:52 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else {
+                result = add(symbol, counter, result);
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+        result = add(symbol, counter, result);
+        return result;
+    }
+
+    public static String add (char sym, int count, String str) {
+        str += Character.toString(sym);
+        if (count > 1) {
+            str += Integer.toString(count);
+        }
+        return str;
+    }
+}
